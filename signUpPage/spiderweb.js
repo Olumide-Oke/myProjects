07bcb6c89db2b1e548dbe88594da477f3b40f335
Olumide Oke <121294635+Olumide-Oke@users.noreply.github.com>
2022-12-23 13:20:39 +0100

    Add files via upload

---
+++ signUpPage/spiderweb.js
@@ -0,0 +1,7 @@
+// const main=document.getElementById("container")
+// console.log(main)
+// const main_class=document.getElementsByClassName("form-heading")
+// console.log( main_class)
+const button=document.querySelector("button")
+button.addEventListener("click", ()=> {
+    alert("submitted")})
